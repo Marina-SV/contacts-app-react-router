85d71a47b58ba607bea790f0f5b5f39b22e72d6c
Senina Marina <frau.senina32@yandex.ru>
2023-02-08 15:59:31 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Index from "./routes/index";
+import {action as destroyAction} from "./routes/destroy";
+
+import './index.css'
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+import Root, {
+  loader as rootLoader,
+  action as rootAction
+} from "./routes/Root";
+
+import ErrorPage from "./ErrorPage";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/Contacts";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    loader: rootLoader,
+    action: rootAction,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        errorElement: <ErrorPage/>,
+        children: [
+          {index: true, element: <Index/>},
+          {
+            path: "contacts/:contactId",
+            element: <Contact/>,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact/>,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact/>,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+          {index: true, element: <Index/>},
+        ],
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      This is my Contacts Book.
+      <br />
+    
+    </p>
+  );
+}
